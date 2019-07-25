c4c4ebd3a19d0fa3164beecba526c3b34470651f
Haja Andriamaro <haja.andri@gmail.com>
2019-07-25 19:01:08 +0200

    some enhancement on css

--- src/AxiosHub/axiosAttachData.js
+++ src/AxiosHub/axiosAttachData.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function axiosWithAuth (name, password, email) {
 
-    const token = 'Bearer LW8faJbmAHacJsOWbifSN5KJqGhe';
+    const token = 'Bearer ATGrB1Td394i8iF4whMxSFZbGQm4';
     const instance = axios.create({
       headers: {
         Authorization: token,
