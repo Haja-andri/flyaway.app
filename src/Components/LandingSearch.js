1b2ce54137f2eee44217c7307ff642f700674826
Haja Andriamaro <haja.andri@gmail.com>
2019-10-25 18:56:28 +0200

    all images aligned and responsive in the card

--- src/Components/LandingSearch.js
+++ src/Components/LandingSearch.js
@@ -1,19 +1,66 @@
 import React from 'react';
-import MainSearchForm from './Forms/MainSearchForm';
+import styled from 'styled-components'
 
 
 export default function LandingHeader(props) {
     return(
-        <div className="header-content">
-        <div className="search-form-container">
-        <MainSearchForm 
-            submitAirport={props.submitAirport} 
-            reRenderWithFlights={props.reRenderWithFlights} 
-            currentMode={props.currentMode}
-        />
-        </div>
-        <div className="showcase">
-        </div>
-        </div>
+        <MainContainer>
+            <MainTittle>
+                <h1>Travel inspiration, from where you are</h1>
+            </MainTittle>
+            <CardContainer>
+                <DestinationCard>
+                    <img src="https://source.unsplash.com/TVyjcTEKHLU/750x1125" />
+                </DestinationCard>
+                <DestinationCard>
+                    <img src="https://source.unsplash.com/zVdf2Hmrcxw/750x1125" />
+                </DestinationCard>
+                <DestinationCard>
+                    <img src="https://source.unsplash.com/gGOzpDZ_Qz0/750x1125" />
+                </DestinationCard>
+                <DestinationCard>
+                    <img src="https://source.unsplash.com/mOEqOtmuPG8/750x1125" />
+                </DestinationCard>
+                <DestinationCard>
+                <img src="https://source.unsplash.com/mZ0sV5KjTVQ/750x1125" />
+                </DestinationCard>
+            </CardContainer>
+        </MainContainer>
     );
 }
+
+const MainContainer = styled.div`
+    border:1px solid red;
+    height:100vh;
+    background:white;
+    padding-top:10%;
+`;
+
+const MainTittle = styled.div`
+    padding-bottom:30px;
+    h1{
+        border:1px solid blue;
+        width:100%
+        text-align:center;
+    }
+`;
+
+const CardContainer = styled.div`
+    display:flex;
+    border:1px solid green;
+    justify-content:center;
+    height:65%;
+`;
+
+const DestinationCard = styled.div`
+    display:flex;
+    border:1px solid yellow;
+    width:25%;
+    max-width:300px;
+    padding:10px;
+    img{
+        width:100%;
+        max-height:450px;
+        border-radius:10px;
+    }
+`;
